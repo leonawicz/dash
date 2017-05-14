a797f67a7e81b705178aa2ae8d6e8470d1f0b3ba
leonawicz <matt.rstat@gmail.com>
2017-05-14 08:18:53 -0600

    Add ui.R

---
+++ climdist/ui.R
@@ -0,0 +1,135 @@
+faq <- source("faq.R", local=TRUE)[[1]]
+
+function(request){
+  dashboardPage(
+    dashboardHeader(
+      title="Climate Explorer",
+      tags$li(class="dropdown",
+        tags$a(href="http://snap.uaf.edu", target="_blank",
+          tags$img(src="SNAP_acronym_100px.png", width="100%", alt="SNAP"), style="padding: 10px; margin: 0px;"))
+      #tags$head(includeScript("ga-nwtapp.js"), includeScript("ga-allapps.js")),
+    ),
+    dashboardSidebar(
+      useToastr(),
+      introjsUI(),
+      #tags$style(HTML(".myijs { min-width: 800px; max-width: 800px; }")),
+      selectInput("mapset", "Change map layer", choices=mapsets, selected=mapsets[8], width="100%"),
+      actionButton("btn_staticmap", "Detailed map", style=action_btn_style, icon("globe")),
+      sidebarMenu(
+        id="tabs",
+        menuItem("Climate", icon=icon("sliders"), tabName="climate"),
+        menuItem("Information", icon=icon("info-circle"), tabName="info")
+      ),
+      conditionalPanel("output.Map != null",
+        actionButton("help", "Take tour", style=action_btn_style, icon=icon("question-circle")),
+        #bookmarkButton(style=action_btn_style)
+        actionButton("fake", "Bookmark", style=action_btn_style, icon=icon("link")), # placeholder
+        bsTooltip("help", "Note: Tour incomplete, under development.", placement="right", options=list(container="body")),
+        bsTooltip("fake", "Note: Server-side bookmarking not yet available.", placement="right", options=list(container="body"))
+      ),
+      #hr(style="margin: 15px;"),
+      #fluidRow(
+      #  column(12, 
+      #    p("Global / semi-global options.", style="text-align: justify; margin: 0px 15px 0px 15px;"),
+      #    selectInput("metric", "Units", c("US", "Metric"), width="100%"),
+      #    selectInput("area_axis_scale", "Axis scale", c("x1", "x100", "x1000"), width="100%")
+      #  )
+      #),
+      #bsTooltip("metric", "Units apply to all data.", placement="right", options=list(container="body")),
+      #bsTooltip("area_axis_scale", "Scale affects plot axes for burn area, fire size and vegetation cover area.",
+      #  placement="right", options=list(container="body")),
+      #uiOutput("download_report")
+      tags$footer(
+        p(a(href="http://snap.uaf.edu", target="_blank",
+          tags$img(src="SNAPDB_acronym_75px.png", width="20%", alt="SNAP Dashboards"), style="padding: 5px; margin: 0px;"),
+        strong(em("SNAP Dashboards"))),
+        style="position:absolute; align: left; bottom:0; width:100%; height:30px; color: white; padding: 5px; z-index: 1000;"
+      )
+    ),
+    dashboardBody(
+      includeCSS("www/styles.css"),
+      bsModal("staticmap", "Fire Management Zones", "btn_staticmap", size="large",
+        img(src='Fire_Mgmt_Areas.png', align="center", style="width: 100%")
+      ),
+      tabItems(
+        tabItem(tabName="climate",
+              box(
+                fluidRow(
+                  column(5,
+                         div(id="plot-container",
+                             leafletOutput("Map", width="100%", height="400px"),
+                             conditionalPanel("output.Map == null", 
+                                              h4("Loading map", style="position: absolute; left: 0; top: 35%; right: 0; text-align: center;"),
+                                              tags$img(src="spinner.gif", id="loading-spinner")
+                             )
+                         )
+                  ),
+                  column(7,
+                         fluidRow(
+                           column(4,
+                             selectInput("variable", "Climate variable", choices=variables, selected=variables[1], width="100%")
+                           ),
+                           column(8,
+                             uiOutput("mapset_regions")
+                           )
+                         ),
+                         fluidRow(
+                           column(4,
+                             selectInput("rcps", "RCP", choices=rcps, selected=rcps[1], multiple=TRUE, width="100%")
+                           ),
+                           column(8,
+                             selectInput("gcms", "GCM", choices=gcms, selected=gcms[1], multiple=TRUE, width="100%")
+                           )
+                         ),
+                         fluidRow(
+                           column(4,
+                             selectInput("seasons", "Season", choices=seasons, selected=seasons[1], multiple=TRUE, width="100%")
+                           ),
+                           column(8,
+                             sliderInput("yrs", "Years", min=period[1], max=period[2], value=c(2006, 2099), step=1, sep="", width="100%")
+                           )
+                         ),
+                         fluidRow(
+                             column(4,
+                               selectInput("marginalize", "Merge distributions across", 
+                                 choices=c("", "RCPs"="RCP", "GCMs"="GCM"), selected="", multiple=TRUE, width="100%")
+                             ),
+                             column(4,
+                               selectInput("clrby", "Color by", choices=c("", "RCP", "GCM", "Season", "Region"), selected="", width="100%")
+                             ),
+                             column(4,
+                               selectInput("fctby", "Facet by", choices=c("", "RCP", "GCM", "Season", "Region"), selected="", width="100%")
+                             )
+                         ),
+                         fluidRow(
+                           column(4, 
+                             actionButton("go_btn", "Build distributions", class="btn-block", icon("globe"))
+                           ),
+                           column(4,
+                             actionButton("settings_btn", "Additional settings", class="btn-block", icon("gear"))
+                           )
+                         )
+                  )
+                ),
+                title="Data selection", width=12, collapsible=TRUE
+              ),
+              fluidRow(
+                column(5,
+                  plotOutput("dist_plot", height="auto")
+                ),
+                column(7,
+                  plotOutput("ts_plot", height="auto")
+                )
+              )
+        ),
+        tabItem(tabName="info",
+          h2("Frequently asked questions"),
+          faq,
+          h2("Contact information"),
+          p("For questions about this application, please email paul.duffy@neptuneinc.org")
+        )
+      )
+    ),
+    title="Climate Explorer"
+  )
+}
