8b175538bf126b4348d95d27a53510a9aa260b8e
leonawicz <matt.rstat@gmail.com>
2017-06-28 14:48:16 -0600

    Improve density plot color, shading and line size. Add annual means points and lines to time series plot as an option via new argument.

--- climdist/plots.R
+++ climdist/plots.R
@@ -5,7 +5,13 @@ distPlot <- function(data, xlb, clrby, clrvec, alpha, fctby, fct_scales, yrs, ty
   ylb <- if(yrs[1]==tail(yrs, 1)) paste(yrs[1], "density") else paste(yrs[1], "-", tail(yrs, 1), "density")
   g <- ggplot(data=data, aes_string("Val"))
   if(type=="density"){
-    g <- g + geom_line(aes_string(colour=clrby), stat="density", alpha=alpha)
+    if(is.null(clrby)){
+      g <- g + geom_density(fill="#A5A5A5", colour="white", alpha=alpha)
+      g <- g + geom_line(colour="#3366FF", size=1, stat="density")
+    } else {
+      g <- g + geom_density(aes_string(fill=clrby), colour="white", alpha=alpha)
+      g <- g + geom_line(aes_string(colour=clrby), size=1, stat="density")
+    }
   } else {
     g <- g + geom_histogram(aes_string(fill=clrby), colour=clr, position="dodge", alpha=alpha)
   }
@@ -14,13 +20,16 @@ distPlot <- function(data, xlb, clrby, clrvec, alpha, fctby, fct_scales, yrs, ty
     
 }
 
-tsPlot <- function(data, ylb, clrby, clrvec, alpha, fctby, fct_scales, show_points, prevent){
+tsPlot <- function(data, ylb, clrby, clrvec, alpha, fctby, fct_scales, ann_means, ann_obs, prevent){
   if(prevent) return()
   lgd_alpha <- guide_legend(override.aes=list(alpha=1))
   pos <- .getPosition(jitter=TRUE, clrby)
   g <- ggplot(data=data, aes_string("Year", "Val", colour=clrby, fill=clrby))
-  if(show_points) g <- g + geom_point(size=3, shape=21, color="black", alpha=alpha, position=pos)
-  g <- g + geom_smooth(method="lm", size=1) + geom_smooth(method="lm", colour="white", size=2, se=FALSE) + geom_smooth(method="lm", size=1, se=FALSE)
+  if(ann_obs) g <- g + geom_point(size=3, shape=21, color="black", alpha=alpha, position=pos)
+  g <- g + geom_smooth(method="lm", size=1) + 
+    geom_smooth(method="lm", colour="white", size=2, se=FALSE) + 
+    geom_smooth(method="lm", size=1, se=FALSE)
+  if(ann_means) g <- g + stat_summary(fun.y=mean, geom="line") + stat_summary(fun.y=mean, geom="point")
   g <- .colorFacet(g, data, clrby, clrvec, fctby, fct_scales)
   g +  theme_bw(base_size=14) + plottheme + labs(y=ylb) + guides(fill=lgd_alpha)
 }
