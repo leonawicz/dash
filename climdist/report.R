146bc8a6dd971bb0f445199e7d1735fb9f87e3d4
leonawicz <matt.rstat@gmail.com>
2017-07-13 18:20:45 -0600

    Add reactives to assist rmarkdown report.

--- climdist/report.R
+++ climdist/report.R
@@ -8,6 +8,42 @@ uni <- reactive({
   x
 })
 
+sbg_to_tables <- function(d, clrby, rnd=0){
+  id <- c("annual", "decadal")
+  clr <- !is.null(clrby)
+  if(clr) lev <- levels(d[[clrby]])
+  x <- purrr::map(id, ~stat_boxes_group(
+    d, clrby, rnd=rnd, type=.x, prevent=FALSE, output="list")) %>%
+    purrr::map(~dplyr::tbl_df(data.frame(do.call(rbind, .x), stringsAsFactors=FALSE)))
+  names(x[[1]])[6] <- "SD"
+  names(x[[2]])[2:5] <- gsub("\\.", " ", names(x[[2]][2:5]))
+  names(x[[2]])[6] <- "Percent change"
+  if(clr){
+    x <- purrr::map(x, ~dplyr::mutate(.x, ColorBy=factor(lev, levels=lev)))
+    names(x[[1]])[1] <- names(x[[2]][1]) <- clrby
+  }
+  names(x) <- id
+  x
+}
+
+report_stats <- reactive({ sbg_to_tables(d(), clrby(), sbArgs()$rnd) })
+annual_plot_content <- reactive({
+  x <- c("Means", "Observations")
+  y <- input$show_annual
+  if(all(x) %in% y) return("means and observations")
+  if(x[1] %in% y) return("means")
+  if(x[2] %in% y) return("observations")
+  "data"
+})
+
+decadal_plot_content <- reactive({
+  x <- c("Box plot", "Strip chart", "Overlay")
+  y <- input$bptype
+  if(y==x[1]) return("box plots")
+  if(y==x[2]) return("strip chart")
+  "box plots with spatial samples overlay"
+})
+
 output$report <- downloadHandler(
   filename="snap_downscaled_climate_custom_report.pdf",
   content=function(file){
@@ -21,12 +57,17 @@ output$report <- downloadHandler(
       units=uni(),
       regions=i()[[3]],
       regions.names=i()[[6]],
+      seasons=input$seasons,
       plot_ts=plot_ts(),
       plot_den=plot_dist(),
       plot_dec=plot_dec(),
       cru=cru,
       clrby=clrby(),
-      fctby=fctby()
+      fctby=fctby(),
+      tables=report_stats(),
+      anncontent=annual_plot_content(),
+      annlm="lm" %in% input$fit_models,
+      deccontent=decadal_plot_content()
     )
     rmarkdown::render(tempReport, output_file=file, params=params, envir=new.env(parent=globalenv())
     )
