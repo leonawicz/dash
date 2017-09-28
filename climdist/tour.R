725e5ceabef6da811168e8eb50413562b747bc36
leonawicz <matt.rstat@gmail.com>
2017-09-28 12:18:37 -0600

    Update tour steps, positioning and class changes.

--- climdist/tour.R
+++ climdist/tour.R
@@ -1,12 +1,13 @@
 tour.text <- read_md_paragraphs("text_tour.txt")
-tour.pos <- c("bottom", "right", "bottom", rep("left", 12), rep("top", 2), "left", rep("top", 2))
+tour.pos <- c(rep("bottom", 13), "top", rep("bottom", 3), rep("top", 2), "bottom", "top")
 tour.element <- c(
   "#controls", "#mapset + .selectize-control", "#Map", "#regions + .selectize-control",
   "#variable + .selectize-control",
   "#rcps + .selectize-control", "#gcms + .selectize-control", "#seasons + .selectize-control", ".js-irs-3", 
-  "#marginalize + .selectize-control", "#clrby + .selectize-control", "#fctby + .selectize-control",
-  "#go_btn", "#settings_btn", "#plot_btn",
-  "#statBoxes1", "#denbox", "#ts_plot", "#statBoxes2", "#dec_plot"
+  "#marginalize + .selectize-control",
+  "#go_btn", "#settings_btn",
+  "#statBoxes1", "#ts_plot", "#clrby_annual + .selectize-control", "#fctby_annual + .selectize-control", 
+  "#dlPlot_annual", "#parsBoxes", "#denbox", "#statBoxes2", "#dec_plot"
 )
 steps <- reactive({ data.frame(element=tour.element, intro=tour.text, position=tour.pos) })
 
@@ -16,8 +17,8 @@ observeEvent(input$help, {
   tour.events <- list(
     "onchange"=I(paste0(
       tour_changeClass(1:20, "info", "climate"),
-      tour_changeClass(1:18, "Decadal", "Annual"),
-      tour_changeClass(19:20, "Annual", "Decadal"),
+      tour_changeClass(1:19, "Decadal", "Annual"),
+      tour_changeClass(20:21, "Annual", "Decadal"),
       collapse="\n"))
   )
   introjs(session, options=tour.options, events=tour.events)
