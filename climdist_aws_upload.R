7964ea8267c9a27f85072fc24e19e1d1c06f131b
leonawicz <matt.rstat@gmail.com>
2017-10-20 15:53:35 -0600

    Update upload script to use the snapprep package aws upload function.

--- climdist_aws_upload.R
+++ climdist_aws_upload.R
@@ -1,11 +1,4 @@
-library(parallel)
-library(aws.s3)
-setwd("/workspace/UA/mfleonawicz/data/clim_2km_seasonal")
+library(snapprep)
 source("/workspace/UA/mfleonawicz/aws_key.R")
-bkt <- "leonawicz"
-files <- list.files(recursive=TRUE)
-aws_upload <- function(file, aws_prefix="apps/ar5_climdist/clim_2km_seasonal", bkt="leonawicz"){
-  print(file)
-  put_object(file, file.path(aws_prefix, file), bkt)
-}
-system.time( mclapply(files, aws_upload, mc.cores=32) )
+aws_upload(snapdef()$ar5dir_dist_monthly_split, "clim/dist/ar5_2km/monthly")
+aws_upload(snapdef()$ar5dir_dist_seasonal, "clim/dist/ar5_2km/seasonal")
